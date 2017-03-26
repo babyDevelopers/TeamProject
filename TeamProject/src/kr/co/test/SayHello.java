ccb1ae6675d21d22298b3477dc54a3563c42c304
KS <dagda@hanafos.com>
2017-03-26 22:30:15 +0900

    업로드

--- TeamProject/src/kr/co/test/SayHello.java
+++ TeamProject/src/kr/co/test/SayHello.java
@@ -5,6 +5,7 @@ public class SayHello {
 		
 		System.out.println("원격 저장소 테스트를 위한 실습입니다. ");
 		System.out.println("충돌 테스트를 위한 로컬 실습입니다. ");
-		System.out.println("원격 저장소의 master branch에서 수정한 내용입니다. ");			
+		System.out.println("원격 저장소의 master branch에서 수정한 내용입니다. ");		
+		
 	}
 }
