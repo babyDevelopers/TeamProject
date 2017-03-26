f8ca367eefe9c69fae900c0e1a2cfcaaae6fcbc9
KS <dagda@hanafos.com>
2017-03-26 16:41:15 +0900

    second 에서 작업후 커밋

--- TeamProject/src/kr/co/test/HelloWorld.java
+++ TeamProject/src/kr/co/test/HelloWorld.java
@@ -3,6 +3,7 @@ package kr.co.test;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello Git World!!!");
+		System.out.println("second brance에서 작업한 내용 ");
 	}
 
 }
